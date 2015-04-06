902e0464e64dd1c39b56484cd52ad4a48e5091b5
zsir <330201816@qq.com>
2015-04-06 22:16:34 +0800

    modify flash_info.py

--- flashInfo/flash_info.py
+++ flashInfo/flash_info.py
@@ -1,31 +1,39 @@
 #----made by zsir----
 
 BLOCK_SIZE = 512
-fp = open('./NANDINFO.nni','rb')
+input_file = open('./NANDINFO.nni','rb')
+output_file = open('./nandinfo.txt','w')
 num = 0
 while True:
-    content = fp.read(BLOCK_SIZE)
+    content = input_file.read(BLOCK_SIZE)
     if not content:
         break
     else:
         nand_id_number = int(content[0x10].encode('hex'), 16)
         for ix in range(nand_id_number):
             if ix == 0:
-                print "nand id:%s" % content[0x11 + ix].encode('hex'),
+                output_file.write("nand id:")
+                output_file.write(content[0x11 + ix].encode('hex'))
+            elif ix == nand_id_number - 1:
+                output_file.write("-")
+                output_file.write(content[0x11 + ix].encode('hex'))
+                output_file.write("\r\n")
             else:
-                print "%s" % content[0x11 + ix].encode('hex'),
+                output_file.write("-")
+                output_file.write(content[0x11 + ix].encode('hex'))
 
         ix = 0x40
-        print "nand vendor:",
+        output_file.write("nand vendor:");
         while int(content[ix].encode('hex'), 16) != 0:
-            print "%s" % content[ix],
+            output_file.write(content[ix])
             ix = ix + 1
+        output_file.write("\r\n")
 
         ix = 0x50
-        print "nand type:",
+        output_file.write("nand type:");
         while int(content[ix].encode('hex'), 16) != 0:
-            print "%s" % content[ix],
+            output_file.write(content[ix])
             ix = ix + 1
-        print ""
+        output_file.write("\r\n\r\n")
         num = num+1
 print num
