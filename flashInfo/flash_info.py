eec6e6a719c9268d108a513d0dc663cbda1b924f
zsir <330201816@qq.com>
2015-04-07 22:54:34 +0800

    Modify flash_info.py to support the specified input and output file

--- flashInfo/flash_info.py
+++ flashInfo/flash_info.py
@@ -1,8 +1,28 @@
 #----made by zsir----
+import sys
+
+argc = len(sys.argv)
+if argc < 2:
+    print "read NANDIFO.nni and write nandinfo.txt"
+    input_file = open('./NANDINFO.nni','rb')
+    output_file = open('./nandinfo.txt','w')
+elif argc == 2:
+    print "read %s and write nandinfo.txt" % sys.argv[1]
+    input_file = open(sys.argv[1],'rb')
+    output_file = open('./nandinfo.txt','w')
+elif argc == 3:
+    print "read %s and write %s" % (sys.argv[1], sys.argv[2])
+    input_file = open(sys.argv[1],'rb')
+    output_file = open(sys.argv[2],'w')
+else:
+    print "usage: python flash_info.py"
+    print "or + input_file"
+    print "or + input_file + output_file"
+    sys.exit()
+
+#print sys.argv[1]
 
 BLOCK_SIZE = 512
-input_file = open('./NANDINFO.nni','rb')
-output_file = open('./nandinfo.txt','w')
 num = 0
 while True:
     content = input_file.read(BLOCK_SIZE)
@@ -36,4 +56,4 @@ while True:
             ix = ix + 1
         output_file.write("\r\n\r\n")
         num = num+1
-print num
+#print num
